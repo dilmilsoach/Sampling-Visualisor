f2ab188e44c11926b694fb413947bbedcf204305
dilmilsoach <dilmilsoach@gmail.com>
2026-05-07 13:18:52 -0300

    Fix string quotes in render_template call

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def index():
 		except Exception as e:
 			print(f"Error: {e}")
 
-	return render_template(index.html, stats=stats, plot_url=plot_url)
+	return render_template('index.html', stats=stats, plot_url=plot_url)
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
